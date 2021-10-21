caa7698235e2d2b92db6be99511ea4a7fffe6c6b
Apon77 <khalakuzzamanapon5@gmail.com>
2021-10-22 01:16:34 +0600

    arrow a12 brinngup

--- cancro.mk
+++ cancro.mk
@@ -46,7 +46,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/vendor/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/vendor/etc/permissions/android.hardware.nfc.xml \
-    $(DEVICE_PATH)/nfc/libnfc-nci.conf:system/etc/libnfc-nci.conf \
     $(DEVICE_PATH)/nfc/libnfc-nci-20791b05.conf:system/etc/libnfc-nci-20791b05.conf \
     $(DEVICE_PATH)/nfc/nfcee_access_debug.xml:system/etc/nfcee_access.xml
 
